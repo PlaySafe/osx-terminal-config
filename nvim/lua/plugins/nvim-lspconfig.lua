74fe2f5b3fa4f0460c06fbb3f93f1b3b41146c2b
Kampanart Saardarewut <kampanart.saardarewut@prudential.co.th>
2022-10-18 22:17:57 +0700

    Update lsp config

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -74,7 +74,7 @@ local lsp_flags = {
 }
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local dap = require('dap')
 local lspconfig = require('lspconfig')
