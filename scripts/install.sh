919453a866c06757cdc12be099fa8e3d2aa1494e
Kampanart Saardarewut <kampanart.saard@gmail.com>
2023-09-03 15:18:43 +0700

    Run script use GNU parallel

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,44 +6,19 @@ source $HOME/.zshrc
 
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install vim
-brew install neovim
-brew install ripgrep
-brew install lua
-brew install luajit
-brew install luarocks
-
-brew install tree
-brew install tree-sitter
-brew install openjdk@17
-sudo ln -sfn /usr/local/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-
-# maven config
-brew install maven
-mkdir -p $HOME/.m2/repository
-cp ./maven/settings.xml $HOME/.m2
-
-# script command
-brew install git
-brew install git-gui
+brew install neovim ripgrep lua luajit luarocks jq tflint ctags-exuberant
+brew install tree tree-sitter parallel wget
+brew install git git-gui
 cp ./script/gitconfig/.gitconfig $HOME/.gitconfig
 cp ./bin/run /usr/local/bin
 
-brew install yamllint
-brew install rust
-brew install pwgen
-
-brew install go
-go install golang.org/x/tools/cmd/goimports@latest
-go install github.com/go-delve/delve/cmd/dlv@latest
-
-brew install python3
-python3 -m pip install --user --upgrade pynvim
-
-brew install jq
-brew install tflint
-brew install ctags-exuberant
+brew install yamllint pwgen
+brew install rust go delve
+brew install python@3.11 && python3 -m pip install --user --upgrade pynvim
 
 #Config the screenshot
 defaults write com.apple.screencapture type jpg
 defaults write com.apple.screencapture location ~/Desktop/Screenshot
+
+brew install maven
+brew install openjdk@17 && sudo ln -sfn /usr/local/opt/openjdk@17/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
